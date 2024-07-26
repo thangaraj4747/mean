a038a02620800300693a7a1e5d928dadc9c51d0d
Thangaraj S <thangaraj.sudalaimani@photon.com>
2024-07-26 11:52:11 +0530

    Registeration completed and favicon issue resolved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
+import favicon from 'serve-favicon';
+import path from 'path';
 import Logging from './library/Logging';
+import UserRoutes from './routes/UserRoutes';
 
 const router = express();
 
@@ -10,26 +13,23 @@ const router = express();
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
-        // Logging.info(`Connected to mongoDB.`);
-        console.log(`Connected to mongoDB.`);
+        Logging.info(`Connected to mongoDB.`);
         startServer();
     })
     .catch((err) => {
-        // Logging.error('Unable to connect.');
-        // Logging.error(err);
-        console.log('Unable to connect.');
-        console.log(err);
+        Logging.error('Unable to connect.');
+        Logging.info(err);
     });
 
 /** Only start the server if Mongo Connects */
 const startServer = () => {
     router.use((req, res, next) => {
         /** Log the Request */
-        console.log(`incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+        Logging.info(`incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
         res.on('finish', () => {
             /** Log the Response */
-            console.log(`incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+            Logging.info(`incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
         });
 
         next();
@@ -41,16 +41,22 @@ const startServer = () => {
     /** Rules of our API */
     router.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Contorl-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
         if (req.method == 'OPTIONS') {
-            res.header('Access-Contorl-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
             return res.status(200).json({});
         }
+
         next();
     });
 
     /** Routes */
+    router.use('/users', UserRoutes);
+
+    // Specify the path to your favicon
+    router.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    // router.get('/favicon.ico', (req, res) => res.status(204));
 
     /** Healthcheck */
     router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
@@ -58,9 +64,9 @@ const startServer = () => {
     /** Error handling */
     router.use((req, res, next) => {
         const error = new Error('not found');
-        console.log(error);
+        Logging.error(error);
         return res.status(400).json({ message: error.message });
     });
 
-    http.createServer(router).listen(config.server.port, () => console.log(`Server is running on port ${config.server.port}.`));
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}.`));
 };
